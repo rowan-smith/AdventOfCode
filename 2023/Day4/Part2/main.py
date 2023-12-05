41c1abba01f8f143da5223068b4e022c96527309
Rowan <rowanpsmith@yahoo.com.au>
2023-12-05 22:09:09 +1000

    Add Day 4, Part 2, 2023

---
+++ 2023/Day4/Part2/main.py
@@ -0,0 +1,20 @@
+from Scratchcards import ScratchCardCollection
+
+SCRATCH_CARDS = ScratchCardCollection()
+SCRATCH_CARDS.load_scratch_cards("example.txt")
+
+
+def main():
+    cards = [1] * len(SCRATCH_CARDS.cards)
+
+    for card_num, card in enumerate(SCRATCH_CARDS.cards):
+        match_count = len(card.find_matches())
+
+        for match_range in range(match_count):
+            cards[card_num + match_range + 1] += cards[card_num]
+
+    print(sum(cards))
+
+
+if __name__ == '__main__':
+    main()
